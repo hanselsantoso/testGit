fd2f1d9278b14041660beb043f5e9af94faa078b
hanselsantoso <hanselsantoso22@gmail.com>
2023-04-13 11:28:47 +0700

    cke cek

--- public/index.php
+++ public/index.php
@@ -9,6 +9,18 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
